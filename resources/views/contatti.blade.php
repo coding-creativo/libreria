8c35a4d93b0a7f2ee7efab1587166cc89fedf230
Cristina <codingcreativo@gmail.com>
2024-05-27 18:07:49 +0200

    intro-laravel

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app') 
+
+@section('title', 'contatti')
+
+@section('content')
+
+<div>
+    <h1>Questa è la pagina contatti</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut natus accusantium possimus nihil inventore! Totam placeat quia dolor minima deleniti cumque fuga, quas distinctio, asperiores excepturi quasi. Eveniet, suscipit debitis.</p>
+</div>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+{{-- Gli diciamo quale layout prendere --}}
+@extends('layouts.app') 
+
+@section('title', 'contatti')
+
+@section('content')
+<div>
+    <h1>Questa è la home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut natus accusantium possimus nihil inventore! Totam placeat quia dolor minima deleniti cumque fuga, quas distinctio, asperiores excepturi quasi. Eveniet, suscipit debitis.</p>
+</div>
+@endsection
